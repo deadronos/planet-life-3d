3980a36d379fecf9e0b86fa95aad1cb22dbe519e
deadronos <iridanblub@hotmail.de>
2025-12-13 22:29:10 +0100

    feat: expand type definitions for OrbitControls and Stars in react-three-drei module

--- src/types/react-three-drei.d.ts
+++ src/types/react-three-drei.d.ts
@@ -1,1 +1,36 @@
-declare module '@react-three/drei';
+declare module '@react-three/drei' {
+  import type React from 'react';
+  import type * as THREE from 'three';
+  import type { ReactThreeFiber } from '@react-three/fiber';
+  import type { OrbitControls as OrbitControlsImpl } from 'three-stdlib';
+
+  type OrbitControlsElementProps = ReactThreeFiber.ThreeElement<typeof OrbitControlsImpl>;
+
+  export type OrbitControlsProps = Omit<OrbitControlsElementProps, 'args'> & {
+    args?: OrbitControlsElementProps['args'];
+    makeDefault?: boolean;
+    camera?: THREE.Camera;
+    domElement?: HTMLElement;
+  };
+
+  export const OrbitControls: React.ForwardRefExoticComponent<
+    OrbitControlsProps & React.RefAttributes<OrbitControlsImpl>
+  >;
+
+  type StarsElementProps = ReactThreeFiber.ThreeElement<typeof THREE.Points>;
+
+  export type StarsProps = Omit<StarsElementProps, 'args'> & {
+    args?: StarsElementProps['args'];
+    radius?: number;
+    depth?: number;
+    count?: number;
+    factor?: number;
+    saturation?: number;
+    fade?: boolean;
+    speed?: number;
+  };
+
+  export const Stars: React.ForwardRefExoticComponent<
+    StarsProps & React.RefAttributes<THREE.Points>
+  >;
+}
